a4abab9984c0c703f31405869b36e463f8fc1600
Ritesh Singh <1124ritesh@gmail.com>
2020-01-30 12:55:04 +0530

    Added Status API

--- src/Paytm.php
+++ src/Paytm.php
@@ -82,3 +82,41 @@ class Paytm
        ];
     }
 }
+
+
+ public function getStatus($orderId)
+    {
+            /* initialize an array */
+            $paytmParams = array();
+
+            /* Find your MID in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys */
+            $paytmParams["MID"] = config('paytm.PAYTM_MERCHANT_MID');
+
+            /* Enter your order id which needs to be check status for */
+            $paytmParams["ORDERID"] = $orderId;
+
+            /**
+            * Generate checksum by parameters we have
+            * Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
+            */
+            $checksum = (new Process)->getChecksumFromArray($paytmParams, config('paytm.PAYTM_MERCHANT_KEY'));
+
+            /* put generated checksum value here */
+            $paytmParams["CHECKSUMHASH"] = $checksum;
+
+            /* prepare JSON string for request */
+            $post_data = json_encode($paytmParams, JSON_UNESCAPED_SLASHES);
+
+            /* for Staging */
+            $url = "https://securegw-stage.paytm.in/order/status";
+
+            /* for Production */
+            // $url = "https://securegw.paytm.in/order/status";
+
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));  
+            return curl_exec($ch);
+    }
